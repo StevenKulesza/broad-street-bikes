03e29540bc9ef8710b1df419e1498771570dc0ef
Steven Kulesza <stevenn.kulesza@gmail.com>
2016-03-30 15:47:51 +0000

    init commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include('partials/header.php'); ?>
+<div class="container">
+	<div class="row text-center">
+		<div class="col-sm-offset-2 col-sm-8">
+			<h1 class="title">Broad Street Bikes</h1>
+		<p>Coming soon to 101 Broad St. Atlanta.</p>
+		<br>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.3367296156657!2d-84.39565968480969!3d33.75196344094168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f50383cb00d0d7%3A0x5b562e47ad480e98!2s101+Broad+St+SW%2C+Atlanta%2C+GA+30303!5e0!3m2!1sen!2sus!4v1458702229013" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+		<hr>
+		<a href="https://www.instagram.com/broadstreetbikes/" target="_blank"><span class="fa-stack fa-lg">
+  <i class="fa fa-circle fa-stack-2x"></i>
+  <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+</span></a>
+		</div>
+	</div>
+</div>
+<?php include('partials/footer.php'); ?>
---
+++ partials/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="en-US">
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:description" name="description" content="Atlanta Bike Shop, Bicycle shop and bike parts.">
+    <meta property="og:title" content="Broad Street Bikes | Atlanta's Bike Shop" />
+    <meta property="og:image" content="" />
+    <link rel="canonical" href="http://broadstreetbikes.com" />
+    <meta name="author" content="Broad Street Bikes">
+    <link rel="icon" type="image/ico" href="favicon.ico">
+    <link rel="shortcut icon" type="image/ico" href="favicon.ico">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <title>Broad Street Bikes | Atlanta Bike Shop, Bicycle sales and bike parts.</title>
+    <!--  CSS -->
+    <link href='https://fonts.googleapis.com/css?family=Oleo+Script' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
